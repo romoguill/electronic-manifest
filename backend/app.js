13d0246aff988bb466153b6a02843c466de1cc90
Rodrigo Moguillanksy <romoguill@gmail.com>
2022-03-02 06:10:36 -0300

    Set up basic server

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}...`);
+});
