c2fde6343d3273f39bd1ee2ac12cfd08dfe380b8
Rodrigo Moguillanksy <romoguill@gmail.com>
2022-03-02 07:17:09 -0300

    Set the env variables and hooked mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 
+const connectDb = require('./database/connect');
 const orderRoute = require('./routes/orderRoute');
 
 const app = express();
@@ -8,6 +10,16 @@ app.use(orderRoute);
 
 port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    console.log(process.env.MONGO_URI);
+    await connectDb(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log('There was an error in setting up the server');
+  }
+};
+
+start();
