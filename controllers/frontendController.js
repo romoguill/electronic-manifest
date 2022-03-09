1754f0e35256da366f4bb521a89b9e0a7c7ddb28
Rodrigo Moguillanksy <romoguill@gmail.com>
2022-03-09 10:02:59 -0300

    Created the views for editing Products and Users. Also hooked the corresponding controllers and routes.

--- controllers/frontendController.js
+++ controllers/frontendController.js
@@ -1,7 +1,23 @@
 function renderIndex(req, res) {
-  res.render('home');
+  res.render('login', {
+    pageTitle: 'Login',
+  });
+}
+
+function renderEditUser(req, res) {
+  res.render('editUser', {
+    pageTitle: 'Edit User',
+  });
+}
+
+function renderEditProduct(req, res) {
+  res.render('editProduct', {
+    pageTitle: 'Edit Product',
+  });
 }
 
 module.exports = {
   renderIndex,
+  renderEditUser,
+  renderEditProduct,
 };
--- routes/frontendRoute.js
+++ routes/frontendRoute.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
-const { renderIndex } = require('../controllers/frontendController');
+const {
+  renderIndex,
+  renderEditUser,
+  renderEditProduct,
+} = require('../controllers/frontendController');
 
 const router = express.Router();
 
 router.get('/', renderIndex);
+
+router.get('/edituser', renderEditUser);
+
+router.get('/editproduct', renderEditProduct);
 
 module.exports = router;
